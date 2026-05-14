299e992020069878547fd1cb79167b55035d59d0
SaphieNyako <mellesanders@protonmail.com>
2026-05-14 16:13:40 +0200

    villager interaction bug

--- src/main/java/com/saphienyako/quest_giver/EventListener.java
+++ src/main/java/com/saphienyako/quest_giver/EventListener.java
@@ -13,6 +13,7 @@ import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.player.Player;
@@ -114,6 +115,9 @@ public class EventListener {
             // Try to see if this entity starts a quest line
             QuestLinkData link = QuestLinkManager.getMatchingLink(questNPC);
             if (link != null) {
+                event.setCancellationResult(InteractionResult.SUCCESS);
+                event.setCanceled(true);
+
                 if(link.backgroundName != null) {
                     QuestGiverAPI.interactQuest(
                             player,
